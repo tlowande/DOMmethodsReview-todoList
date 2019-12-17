be8c1c18336d71d977a4e2388f4afca2f7a102ac
Tamires <tamires.lowande@gmail.com>
2019-12-17 13:59:59 -0500

    adds style when task is completed

--- external.js
+++ external.js
@@ -14,10 +14,14 @@ window.onload = function () {
   // const todosArray = []
   const todosArray = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : []
 
+  const taskComplete = localStorage.getItem('taskComplete') ? JSON.parse(localStorage.getItem('taskComplete')) : []
+
   // use localStorage setItem() method to set `todos` to a string with `JSON.stringify()`
   // JSON.stringify() if used because localStorage works with strings. This is how
   // we sent data to localStorage.
   localStorage.setItem('todos', JSON.stringify(todosArray))
+
+  localStorage.setItem('taskComplete', JSON.stringify(taskComplete))
 
   // Declare a variable `storage` that contains all the information in localStorage.
   // We will assign to `storage` JSON.parse() method that converts strings from local
@@ -25,6 +29,7 @@ window.onload = function () {
   // When receiving data from a web server, the data is always a string.
   //Parse the data with JSON.parse(), and the data becomes a JavaScript object.
   const storage = JSON.parse(localStorage.getItem('todos'))
+  const status = JSON.parse(localStorage.getItem('taskComplete'))
 
   // Step 2 -> attach an event listener to the `form` variable with `addEventListener`
   // to capture the user input on the `submit` event.
@@ -37,9 +42,10 @@ window.onload = function () {
 
     // push onto `todosArray` the `input.value`
     todosArray.push(input.value)
-
+    taskComplete.push("false")
     // on localStorage now use `setItem()` for the key `'todos'` the value of the todosArray as a string with the `JSON.stringify()` method.
     localStorage.setItem('todos', JSON.stringify(todosArray))
+    localStorage.setItem('taskComplete', JSON.stringify(taskComplete))
 
     todoMaker(input.value)
     input.value = ""
@@ -48,27 +54,45 @@ window.onload = function () {
   // Step 3 -> create a todoMaker function that creates 'li' elements with the text user provides
   // from their form and appends it to the 'ul'.
   const todoMaker = (value) => {
+    let container = document.createElement("div");
     let todoItem = document.createElement("li");
     todoItem.textContent = value;
-    const icon = document.createElement("i")
-    icon.setAttribute("class", "fas fa-times-circle")
-
-    todoItem.appendChild(icon)
-    todoList.appendChild(todoItem)
-
-    icon.addEventListener("click", () => {
-      todoItem.parentNode.removeChild(todoItem)
-      let item = JSON.parse(localStorage.getItem('todos'))
-      console.log(item.indexOf(value))
-      item.splice(item.indexOf(value), 1)
-      localStorage.setItem('todos', JSON.stringify(item))
+    console.log(taskComplete[todosArray.indexOf(value)])
+    if (taskComplete[todosArray.indexOf(value)] === "true") {
+      todoItem.classList.add('complete')
+    }
+    todoItem.addEventListener("click", () => {
+      if (todoItem.classList.contains('complete')) {
+        todoItem.classList.remove('complete')
+        taskComplete.splice(todosArray.indexOf(value), 1, "false")
+        localStorage.setItem('taskComplete', JSON.stringify(taskComplete))
+      } else {
+        todoItem.classList.add('complete')
+        taskComplete.splice(todosArray.indexOf(value), 1, "true")
+        localStorage.setItem('taskComplete', JSON.stringify(taskComplete))
+      }
+      console.log(taskComplete)
     })
 
-    // todoList.appendChild(icon)
+    const icon = document.createElement("i")
+    icon.setAttribute("class", "fas fa-times-circle")
+    icon.addEventListener("click", () => {
+      taskComplete.splice(todosArray.indexOf(value), 1)
+      // console.log(taskComplete)
+      todosArray.splice(todosArray.indexOf(value), 1)
+      localStorage.setItem('todos', JSON.stringify(todosArray))
+      localStorage.setItem('taskComplete', JSON.stringify(taskComplete))
+      container.parentNode.removeChild(container)
+    })
+
+    container.appendChild(todoItem)
+    container.appendChild(icon)
+    todoList.appendChild(container)
+
 
   }
 
-  storage.map(item => todoMaker(item))
+  todosArray.map(item => todoMaker(item))
 
   // Step 4 -> attach an event listener to the `clear all` button listening for
   // a user click.
