4b271f3af31de0591dcc5b8c59ea39a9ec44bee4
Tamires <tamires.lowande@gmail.com>
2020-01-04 15:58:47 -0500

    refactors code and comments

--- external.js
+++ external.js
@@ -1,8 +1,5 @@
 window.onload = function () {
 
-  // Step 1 -> create variables `form`, `todosList`, `button`, `input`
-  // to target the form, unordered list, button and input.
-
 
   const form = document.querySelector("form");
   const todoList = document.querySelector("ul");
@@ -13,34 +10,12 @@ window.onload = function () {
 
   const todosArray = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : []
 
-  // const taskComplete = localStorage.getItem('taskComplete') ? JSON.parse(localStorage.getItem('taskComplete')) : []
-
-  // use localStorage setItem() method to set `todos` to a string with `JSON.stringify()`
-  // JSON.stringify() if used because localStorage works with strings. This is how
-  // we sent data to localStorage.
   localStorage.setItem('todos', JSON.stringify(todosArray))
 
-  // localStorage.setItem('taskComplete', JSON.stringify(taskComplete))
+  //____________________
 
-  // Declare a variable `storage` that contains all the information in localStorage.
-  // We will assign to `storage` JSON.parse() method that converts strings from local
-  // storage into data we can access with JavaScript.
-  // When receiving data from a web server, the data is always a string.
-  //Parse the data with JSON.parse(), and the data becomes a JavaScript object.
-  const storage = JSON.parse(localStorage.getItem('todos'))
-  const status = JSON.parse(localStorage.getItem('taskComplete'))
-
-  // Step 2 -> attach an event listener to the `form` variable with `addEventListener`
-  // to capture the user input on the `submit` event.
-  // Make sure to run `preventDefault()` on the event when the form is submitted.
-  // Call a `todoMaker` function which we will create in step three and pass to it
-  // the `input` variable and target the value the user has provide with `input.value`.
-  // Finally, set the `input.value` to an empty string.const form = document.querySelector("form");
   form.addEventListener("submit", e => {
     e.preventDefault()
-
-    // push onto `todosArray` the `input.value`
-    // todosArray.push(input.value)
 
     let todo = {
       "title": input.value,
@@ -49,10 +24,8 @@ window.onload = function () {
       "date": date.value
     }
     todosArray.push(todo)
-    // taskComplete.push("false")
-    // on localStorage now use `setItem()` for the key `'todos'` the value of the todosArray as a string with the `JSON.stringify()` method.
+
     localStorage.setItem('todos', JSON.stringify(todosArray))
-    // localStorage.setItem('taskComplete', JSON.stringify(taskComplete))
 
     todoMaker(todo)
     input.value = ""
@@ -60,8 +33,8 @@ window.onload = function () {
     date.value = ""
   })
 
-  // Step 3 -> create a todoMaker function that creates 'li' elements with the text user provides
-  // from their form and appends it to the 'ul'.
+  //____________________
+
   const todoMaker = (value) => {
 
     let container = document.createElement("div");
